de884def30324b683f136f1c860b32c101a903d3
Valentyn Mostovyi <mostovoy.v@gmail.com>
2024-11-27 15:23:11 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/* Остановка карусели 
+var myCarousel = document.querySelector('#carousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: false
+});
+var myCarousel = document.querySelector('#carousel-sidebar')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: false
+});
+*/
+
+/* Вызов слайдера внизу */
+$('#elastislide').elastislide();
